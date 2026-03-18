ba290d97d74cd6fdb32ed58c373b516bcb02a54c
ngoc-2912 <ntnn29122006@gmail.com>
2026-03-18 16:04:11 +0700

    add các thành phần cần thiết cho dự án - Như Ngọc

---
+++ src/app/layouts/client-layout/client-layout.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-layout',
+  imports: [],
+  templateUrl: './client-layout.html',
+  styleUrl: './client-layout.scss',
+})
+export class ClientLayout {}
